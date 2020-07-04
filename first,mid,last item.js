fc59fecbbb3b9073940ced743a69048a869c2a29
niranjana94 <60933884+niranjana94@users.noreply.github.com>
2020-07-04 19:58:09 +0530

    Add files via upload
    Get the first item, the middle item and the last item of the array

---
+++ first,mid,last item.js
@@ -0,0 +1,6 @@
+const friends = ['Mari','MaryJane','CaptianAmerica','Munnabai','Jeff','AAKchandran'];
+
+console.log("First Item "+ friends[0]);
+var middleItem = friends.length/2 ;
+console.log("Middle Item " + friends[middleItem]);
+console.log("Last Item " + friends[friends.length -1]);

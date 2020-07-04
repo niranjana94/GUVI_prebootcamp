ed86d005aaad206bd79f8370e52dcc5729a8ee8c
niranjana94 <60933884+niranjana94@users.noreply.github.com>
2020-07-04 19:40:03 +0530

    circumference and area of circle
    Given their radius of a circle and find its diameter, circumference and area.

---
+++ area circumf circle.js
@@ -0,0 +1,11 @@
+var radius = parseFloat(userInput[0]);
+
+var diameter = 2* radius;
+
+var circumf = 2*3.14*radius;
+
+var area = 3.14*radius*radius;
+
+console.log("Diameter "+diameter );
+console.log("Circumference "+circumf.toFixed(2) );
+console.log("Area of circle "+area.toFixed(2) );

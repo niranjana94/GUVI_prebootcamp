f0a8884630232099d5de545ceef25692503a1c77
niranjana94 <60933884+niranjana94@users.noreply.github.com>
2020-07-04 19:05:37 +0530

    Add files via upload
    Declare variables to store your first name, last name, marital status, country and age in a single line

---
+++ Declare variable in single line.js
@@ -0,0 +1,3 @@
+var firstName,lastName,maritalStatus,country,age;
+
+console.log("First Name "+firstName + "," +"Last Name " + lastName + "," +"Marital status " + maritalStatus + "," +"Country "+ country + "," +"Age " + age);

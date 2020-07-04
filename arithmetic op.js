07f9edddc153266ab9f29adc808f7fda0428d07a
niranjana94 <60933884+niranjana94@users.noreply.github.com>
2020-07-04 19:40:37 +0530

    arithmetic operations
    Given two numbers and perform all arithmetic operations.

---
+++ arithmetic op.js
@@ -0,0 +1,16 @@
+var num1 = parseFloat(userInput[0]);
+
+var num2 = parseFloat(userInput[1]);
+
+var add = num1+num2;
+
+var sub = num1-num2;
+
+var mul = num1*num2;
+
+var div = num1/num2;
+
+console.log(num1 +"+"+num2 +"=" + add );
+console.log(num1 +"-"+num2 +"=" + sub );
+console.log(num1 +"*"+num2 +"=" + mul );
+console.log(num1 +"/"+num2 +"=" + div );

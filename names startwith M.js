2251916aa0199bf7b4735ec764c15905af6f6e46
niranjana94 <60933884+niranjana94@users.noreply.github.com>
2020-07-04 20:02:42 +0530

    Add files via upload
    Find the names and return the list starting with letter M.

---
+++ names startwith M.js
@@ -0,0 +1,9 @@
+const friends = ['Mari','MaryJane','CaptianAmerica','Munnabai','Jeff','AAKchandran'];
+
+for(var i = 0;i<friends.length ; i++)
+{
+    if(friends[i].startsWith('M'))
+    {
+        console.log(friends[i]);
+    }
+}

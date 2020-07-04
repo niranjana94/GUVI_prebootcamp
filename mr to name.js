804de6e488a0ff69c10fed106f446f6bfcc64262
niranjana94 <60933884+niranjana94@users.noreply.github.com>
2020-07-04 20:00:03 +0530

    Add files via upload
    Add Mr or Ms to the names in the friends array.

---
+++ mr to name.js
@@ -0,0 +1,8 @@
+const friends = ['Mari','MaryJane','CaptianAmerica','Munnabai','Jeff','AAKchandran'];
+
+for(var i = 0 ;i<friends.length ; i++)
+{
+ friends[i] = "Mr."+friends[i];
+}
+
+console.log(friends);

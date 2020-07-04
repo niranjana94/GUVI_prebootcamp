deeb2f3d3dec336a5fb0b336e25c987d4d20c40e
niranjana94 <60933884+niranjana94@users.noreply.github.com>
2020-07-04 23:34:28 +0530

    Change the code to print all the gifts
    Change the code to print all the gifts
    fix.html
    <!DOCTYPE html>
    <html>
    <body>
     <script src=”script.js”></script>
    </body>
    </html>
    script.js
    let gifts = ["teddy bear", "drone", "doll"];
    for (let i = 0; i < 3; i++) {
      console.log('Wrapped ${'gifts[i]'} and added a bow!');
    }

---
+++ script17.js
@@ -0,0 +1,5 @@
+let gifts = ["teddy bear", "drone", "doll"];
+for (let i = 0; i < 3; i++) {
+ // console.log("Wrapped ${'gifts[i]'} and added a bow!");
+  console.log(`Wrapped ${gifts[i]} and added a bow!`);
+}
